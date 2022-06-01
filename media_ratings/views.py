5988d981267aa0ac5a7ef9d0425c0a92720ddbe9
Manuel <manuelpinedacabeza@gmail.com>
2022-06-01 18:21:44 +0100

    Added homepage, scoreboard and scoreboard_data views.

--- media_ratings/views.py
+++ media_ratings/views.py
@@ -1,3 +1,45 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from .utils import get_data_from_json
+from .forms import SearchForm
 
 # Create your views here.
+
+def homepage(request):
+    template = "search.html"
+    form = SearchForm()
+
+    context = {'form': form}
+    return render(request, template, context)
+
+def scoreboard(request):
+    search_term = request.POST.get('search')
+    template = "scoreboard.html"
+
+    context = {
+        "search_term": search_term,
+    }
+    return render(request, template, context)
+
+def scoreboard_data(request):
+    search_term  = request.GET.get("media")
+    mock_data = get_data_from_json("media_ratings/mock-data.json")
+
+    if search_term == "stranger-things":
+        mock_data = mock_data["ratingAgencies"][0]
+    else:
+        mock_data = mock_data["ratingAgencies"][1]
+    return JsonResponse(mock_data)
+
+
+
+# def search_result(request):
+#     template = "results.html"
+#     search_term = request.POST.get('search')
+
+#     context = {}
+#     return render(request, template, context)
+
+# api_key = "k_oj5mw37f"
+# series_id = "tt4574334" # Stranger Things.
+# request_url = f"https://imdb-api.com/en/API/SeasonEpisodes/{api_key}/{series_id}/1"
