aa6474a9798ac7535c536eda870d2afe0415dd8b
Manuel <manuelpinedacabeza@gmail.com>
2022-06-09 22:29:19 +0100

    Renamed get_data_from_json to load_json_data. Added save_json_data, capitalize_phrase, sanitize_phrase  standardize_phrase and start_crawler_process

--- media_ratings/utils.py
+++ media_ratings/utils.py
@@ -1,6 +1,33 @@
 import json
 
-def get_data_from_json(file):
+def load_json_data(file):
     with open(file) as f:
       data = json.load(f)
     return data
+
+
+def save_json_data(file, data):
+    with open(file, 'w') as f:
+        json.dump(data, f)
+        
+
+def capitalize_phrase(phrase):
+    return " ".join([word.capitalize() for word in phrase.split(" ")])
+
+def sanitize_phrase(phrase):
+    return "".join(char.replace("&", "and").replace("-", " ") for char in phrase)
+
+def standardize_phrase(phrase):
+    return capitalize_phrase(sanitize_phrase(phrase))
+
+def start_crawler_process(spider):
+    from scrapy.crawler import CrawlerProcess
+    from scrapy.utils.project import get_project_settings
+
+    settings = get_project_settings()
+    print(settings)
+    print("------------------------------------------------------------")
+    process = CrawlerProcess(settings)
+    process.crawl(spider)
+    process.start()
+    
