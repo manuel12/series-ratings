2f2c467d97c1edd92530af72e4c4c979470dbe95
Manuel <manuelpinedacabeza@gmail.com>
2022-06-09 22:30:03 +0100

    Added models for Media, TV_Series IMDb and Rottentomatoes scores

--- media_ratings/models.py
+++ media_ratings/models.py
@@ -1,3 +1,68 @@
 from django.db import models
 
 # Create your models here.
+
+class Media(models.Model):
+    """Base class for representing a media instance.
+    Should not be used directly and extended instead."""
+    
+    title = models.CharField(max_length=100, unique=True, blank=False, null=False)
+    description = models.TextField(blank=True)
+    
+    def imdb_scores(self):
+        imdb_score_instance = IMDbScores.objects.filter(media=self).first()
+        
+        if(imdb_score_instance):
+            print(f"IMDb score instance found for media: {self}")
+            return {
+                "imdb_score": f"{imdb_score_instance.imdb_score}/10"
+            }
+        else:
+            return {
+                "imdb_score":   "N/A"
+            }
+    
+    def rottentomatoes_scores(self):
+        rt_score_instance = RottentomatoesScores.objects.filter(media=self).first()
+        
+        if(rt_score_instance):
+            print(f"RT score instance found for media: {self}")
+            return {
+                "tomatometer": f"{rt_score_instance.tomatometer_score}%",
+                "audience_score": f"{rt_score_instance.audience_score}%"
+            }
+        else:
+            return {
+                "tomatometer": "N/A",
+                "audience_score": "N/A"
+            }
+    
+        
+    def __str__(self):
+        return self.title
+        
+
+class TV_Series(Media):
+    num_seasons = models.IntegerField(blank=True, default=1)
+    num_eps_per_season = models.IntegerField(blank=True, default=1)
+    num_eps = models.IntegerField(blank=True, default=1)
+    
+    
+class MediaScore(models.Model):
+    """Base class for representing a media scores.
+    Should not be used directly and extended instead."""
+    
+    media = models.ForeignKey(Media, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{Media} scores"
+    
+    
+class IMDbScores(MediaScore):
+    imdb_score = models.FloatField(blank=True)
+    
+    
+class RottentomatoesScores(MediaScore):
+    tomatometer_score = models.IntegerField(blank=True)
+    audience_score = models.IntegerField(blank=True)
+
