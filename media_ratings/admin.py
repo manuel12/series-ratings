4896dcf947f81c76f24125796da871daaed2c100
Manuel <manuelpinedacabeza@gmail.com>
2022-06-09 22:30:26 +0100

    Added different admin configs

--- media_ratings/admin.py
+++ media_ratings/admin.py
@@ -1,3 +1,27 @@
+from pydoc import describe
 from django.contrib import admin
+from media_ratings.models import *
 
 # Register your models here.
+
+class MediaAdmin(admin.ModelAdmin):
+    list_display = ["title", "description"]
+    
+class TV_SeriesAdmin(admin.ModelAdmin):
+    list_display = ["title", "description", "num_seasons", "num_eps_per_season", "num_eps"]
+    
+class MediaScoreAdmin(admin.ModelAdmin):
+    list_display = ["media"]
+    
+class IMDbScoresAdmin(admin.ModelAdmin):
+    list_display = ["media", "imdb_score"]
+    
+class RottentomatoesScoresAdmin(admin.ModelAdmin):
+    list_display = ["media", "tomatometer_score", "audience_score"]
+    
+
+admin.site.register(Media, MediaAdmin)
+admin.site.register(TV_Series, TV_SeriesAdmin)
+admin.site.register(MediaScore, MediaScoreAdmin)
+admin.site.register(IMDbScores, IMDbScoresAdmin)
+admin.site.register(RottentomatoesScores, RottentomatoesScoresAdmin)
