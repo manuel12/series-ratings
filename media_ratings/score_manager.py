6b49f2f940036bd0ea6a219e4e5f2bc574c4c7fe
Manuel <manuelpinedacabeza@gmail.com>
2022-06-13 20:34:48 +0100

    Added, fetch_score_helper, create_score_model_instance_helper, formatted code

--- media_ratings/score_manager.py
+++ media_ratings/score_manager.py
@@ -1,6 +1,6 @@
 from .utils import standardize_phrase
 from .models import TV_Series, IMDbScores, RottentomatoesScores
-from .search_parser import IMDBSearchResultsParser, RottentomatoesSearchResultsParser
+from .search_parsers import IMDBSearchResultsParser, RottentomatoesSearchResultsParser
 from .parsers import IMDbMediaPageParser, RottentomatoesMediaPageParser
 
 
@@ -9,23 +9,24 @@ class ScoreManager():
     Class to handle the process of standardizing user's search term, 
     creating a tv series instance if it doesn't already exist, fetching 
     said series's search result urls on IMDb or Rottentomatoes.
-    
+
     Fetching their score values, creating imdb or rt score model
     instances and saving them to the database.
-    
+
     And finally returning a dictionary of all the score values.
     """
-    
+
     def __init__(self, search_term):
         self.imdb_score = "N/A"
         self.rt_tomatometer = "N/A"
         self.rt_audience_score = "N/A"
-        
+        self.complete_data_available = False
+
         self.search_term = standardize_phrase(search_term)
         print(f"-- Searching for term: {self.search_term}")
 
         tv_series = TV_Series.objects.filter(title=self.search_term).first()
-        
+
         if tv_series:
             print(f"-- Found series: {tv_series}")
 
@@ -40,12 +41,9 @@ class ScoreManager():
 
             if "N/A" not in (self.imdb_score, self.rt_tomatometer, self.rt_audience_score):
                 self.complete_data_available = True
-            else:
-                self.complete_data_available = False
         else:
             self.tv_series = TV_Series.objects.create(title=self.search_term)
             self.tv_series.save()
-            self.complete_data_available = False
 
         print(f"-- complete_data_available: {self.complete_data_available}")
 
@@ -74,7 +72,7 @@ class ScoreManager():
 
                 # Save imdb model instance to db.
                 self.save_model_instance(new_imdb_score)
-                score_data["imdb"] = imdb_score_value
+                score_data["imdb"] = f"{imdb_score_value}/10"
 
             if "N/A" in [self.rt_tomatometer, self.rt_audience_score]:
                 print(f"-- Fetching score data for rt")
@@ -89,40 +87,48 @@ class ScoreManager():
 
                 # Save rt model instance to db.
                 self.save_model_instance(new_rt_score)
-                score_data["rt"]["tomatometer"] = rt_score_values["tomatometer"]
-                score_data["rt"]["audience_score"] = rt_score_values["audience_score"]
+                score_data["rt"]["tomatometer"] = f"{rt_score_values['tomatometer']}%"
+                score_data["rt"]["audience_score"] = f"{rt_score_values['audience_score']}%"
 
             return score_data
 
     def fetch_score(self, agency):
         if agency == "imdb":
-            search_parser = IMDBSearchResultsParser(self.search_term)
-            search_result_url = search_parser.get_search_result_url()
-            page_parser = IMDbMediaPageParser(search_result_url)
-            score_value = page_parser.get_score_value()
-            return score_value
+            return self.fetch_score_helper(agency,
+                                           IMDBSearchResultsParser,
+                                           IMDbMediaPageParser)
         else:
-            search_parser = RottentomatoesSearchResultsParser(self.search_term)
-            search_result_url = search_parser.get_search_result_url()
-            page_parser = RottentomatoesMediaPageParser(search_result_url)
+            return self.fetch_score_helper(agency,
+                                           RottentomatoesSearchResultsParser,
+                                           RottentomatoesMediaPageParser)
+
+    def fetch_score_helper(self, agency, search_result_parser, media_page_parser):
+        search_parsers = search_result_parser(self.search_term)
+        search_result_url = search_parsers.get_search_result_url()
+        page_parser = media_page_parser(search_result_url)
+        if agency == "rt":
             score_values = {}
             score_values["tomatometer"] = page_parser.get_tomatometer_value()
             score_values["audience_score"] = page_parser.get_audience_score_value()
             return score_values
+        score_value = page_parser.get_score_value()
+        return score_value
 
     def create_score_model_instance(self, agency, score_value_one, score_value_two=None):
         if agency == "imdb":
-            new_score = IMDbScores.objects.create(
-                media=self.tv_series,
-                imdb_score=score_value_one)
+            new_score = self.create_score_model_instance_helper(IMDbScores,
+                                                                media=self.tv_series,
+                                                                imdb_score=score_value_one)
         else:
-            new_score = RottentomatoesScores.objects.create(
-                media=self.tv_series,
-                tomatometer_score=score_value_one,
-                audience_score=score_value_two)
-
+            new_score = self.create_score_model_instance_helper(RottentomatoesScores,
+                                                                media=self.tv_series,
+                                                                tomatometer_score=score_value_one,
+                                                                audience_score=score_value_two)
         print(f"-- Created new {agency} score...")
         return new_score
+
+    def create_score_model_instance_helper(self, scores_model, *args, **kwargs):
+        return scores_model.objects.create(*args, **kwargs)
 
     def save_model_instance(self, model_instance):
         model_instance.save()
