c3443443235744a913c040562f08452dbd693caa
Manuel <manuelpinedacabeza@gmail.com>
2022-06-13 20:36:03 +0100

    Added try/except around search term. Added try/except around get_score_data.

--- media_ratings/views.py
+++ media_ratings/views.py
@@ -1,5 +1,5 @@
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import SearchForm
 from .utils import capitalize_phrase, sanitize_phrase
 from .score_manager import ScoreManager
@@ -19,8 +19,13 @@ def scoreboard(request):
     search_term = request.POST.get('search')
     template = "scoreboard.html"
 
-    capitalized_search_term = capitalize_phrase(search_term)
-    sanitized_search_term = sanitize_phrase(search_term)
+    try:
+        capitalized_search_term = capitalize_phrase(search_term)
+        sanitized_search_term = sanitize_phrase(search_term)
+        print(sanitized_search_term)
+    except AttributeError:
+        return redirect("media_ratings:home")
+
     context = {
         "media_title": capitalized_search_term,
         "search_term": sanitized_search_term
@@ -30,7 +35,18 @@ def scoreboard(request):
 
 def fetch_score_data(request):
     search_term = request.GET.get("media")
-    
+
     sc = ScoreManager(search_term)
-    score_data = sc.get_score_data()
+    try:
+        score_data = sc.get_score_data()
+        print(score_data)
+    except Exception as e:
+        print(e)
+        score_data = {
+            "rt": {
+                "tomatometer": "N/A",
+                "audience_score": "N/A"
+            },
+            "imdb": "N/A"
+        }
     return JsonResponse(score_data)
