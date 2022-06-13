73174f9ea5fd259da4de3b513312f9f9457b7845
Manuel <manuelpinedacabeza@gmail.com>
2022-06-13 20:32:35 +0100

    Remove results url

--- media_ratings/urls.py
+++ media_ratings/urls.py
@@ -5,7 +5,6 @@ app_name = 'media_ratings'
 
 urlpatterns = [
     path('', views.homepage, name='home'),
-    #path('results/', views.search_result, name='result'),
     path('scoreboard/', views.scoreboard, name='scoreboard'),
     path('fetch-score-data/', views.fetch_score_data, name='fetch-score-data')
 ]
