41a2c862e25d10a2ab8c6cd6f6b90da5e67ee8f9
Manuel <manuelpinedacabeza@gmail.com>
2022-06-13 20:33:11 +0100

    Added raise_attribute_error

--- media_ratings/utils.py
+++ media_ratings/utils.py
@@ -1,33 +1,31 @@
 import json
 
+
 def load_json_data(file):
     with open(file) as f:
-      data = json.load(f)
+        data = json.load(f)
     return data
 
 
 def save_json_data(file, data):
     with open(file, 'w') as f:
         json.dump(data, f)
-        
+
 
 def capitalize_phrase(phrase):
+    raise_attribute_error(phrase)
     return " ".join([word.capitalize() for word in phrase.split(" ")])
 
+
 def sanitize_phrase(phrase):
+    raise_attribute_error(phrase)
     return "".join(char.replace("&", "and").replace("-", " ") for char in phrase)
+
 
 def standardize_phrase(phrase):
     return capitalize_phrase(sanitize_phrase(phrase))
 
-def start_crawler_process(spider):
-    from scrapy.crawler import CrawlerProcess
-    from scrapy.utils.project import get_project_settings
 
-    settings = get_project_settings()
-    print(settings)
-    print("------------------------------------------------------------")
-    process = CrawlerProcess(settings)
-    process.crawl(spider)
-    process.start()
-    
+def raise_attribute_error(phrase):
+    if not phrase:
+        raise AttributeError("The search term cannot be empty.")
