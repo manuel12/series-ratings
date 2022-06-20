c3f4d0734e8b35a9a9363147565da76ccee70aff
Manuel <manuelpinedacabeza@gmail.com>
2022-06-20 19:15:55 +0100

    Refactored tests

--- media_ratings/tests/test_parser.py
+++ media_ratings/tests/test_parser.py
@@ -1,88 +1,91 @@
 from django.test import TestCase
 from media_ratings.parsers import Parser
 
+html_page = """
+<html>
+    <head><title>Mock Page</title></head>
+    <body>
+        <div class="score-element-one" data-test="rating-score-one">
+            9.7/10
+        </div>
+        <div>
+            <span class="score-element-two-wrapper">
+                <div class="score-element-two">
+                    93%
+                </div>
+            </span>
+        </div>
+    </body>
+</html>
+"""
+
 
 class ParserTests(TestCase):
     def setUp(self):
-        self.google_url = "https://www.google.com/"
-        self.imdb_url = "https://www.imdb.com/title/tt9561862/"  # love, death and robots
-        # love, death and robots
-        self.rt_url = "https://www.rottentomatoes.com/tv/love_death_robots"
+        self.parser = Parser(html_page=html_page)
 
     def test_url_cannot_be_false(self):
         with self.assertRaises(TypeError):
             url = False
             parser = Parser(url)
 
-    def test_page_source(self):
-        url = "https://www.google.com/"
-        parser = Parser(url)
-        self.assertInHTML("<title>Google</title>", str(parser._get_page_source()))
+    def test_get_page_source(self):
+        self.assertInHTML("<title>Mock Page</title>",
+                          str(self.parser._get_page_source()))
 
     def test_soup(self):
-        url = "https://www.google.com/"
-        parser = Parser(url)
-        self.assertIn("<title>Google</title>", str(parser.soup))
+        self.assertIn("<title>Mock Page</title>", str(self.parser.soup))
 
     def test_get_elem_1(self):
-        parser = Parser(self.imdb_url)
-        parser.elem_class_1 = {"tag": "div",
-                                     "class": "sc-7ab21ed2-2 kYEdvH"}
+        self.parser.elem_class_1 = {"tag": "div",
+                                    "class": "score-element-one"}
 
-        self.assertIn("hero-rating-bar__aggregate-rating__score",
-                      str(parser.get_elem_1()))
-        
+        self.assertIn("rating-score-one",
+                      str(self.parser.get_elem_1()))
+
     def test_elem_class_1_not_assigned_error(self):
         with self.assertRaises(AttributeError):
-            parser = Parser(self.imdb_url)
-            parser.get_elem_1()
+
+            self.parser.get_elem_1()
 
     def test_get_elem_1_returns_none_when_elem_not_found(self):
-        parser = Parser(self.imdb_url)
-        parser.elem_class_1 = {"tag": "div",
-                                     "class": "non-existant-class"}
-        self.assertIsNone(parser.get_elem_1())
-
+        self.parser.elem_class_1 = {"tag": "div",
+                                    "class": "non-existant-class"}
+        self.assertIsNone(self.parser.get_elem_1())
 
     def test_get_value_1(self):
-        parser = Parser(self.imdb_url)
-        parser.elem_class_1 = {"tag": "div",
-                                     "class": "sc-7ab21ed2-2 kYEdvH"}
+        self.parser.elem_class_1 = {"tag": "div",
+                                    "class": "score-element-one"}
 
-        self.assertEqual(parser.get_value_1(), "8.4/10")
-    
+        self.assertEqual(self.parser.get_value_1(), "9.7/10")
+
     def test_get_value_1_returns_none_when_elem_not_found(self):
-        parser = Parser(self.imdb_url)
-        parser.elem_class_1 = {"tag": "div",
-                                     "class": "non-existant-class"}
-        self.assertIsNone(parser.get_value_1())
+        self.parser.elem_class_1 = {"tag": "div",
+                                    "class": "non-existant-class"}
+        self.assertIsNone(self.parser.get_value_1())
 
     def test_get_elem_2(self):
-        parser = Parser(self.rt_url)
-        parser.elem_class_2 = {"tag": "span",
-                                     "class": "mop-ratings-wrap__percentage"}
-        self.assertIn("mop-ratings-wrap__percentage", str(parser.get_elem_2()))
+        self.parser.elem_class_2 = {"tag": "span",
+                                    "class": "score-element-two-wrapper"}
+        self.assertIn("score-element-two", str(self.parser.get_elem_2()))
 
     def test_elem_class_2_not_assigned_error(self):
         with self.assertRaises(AttributeError):
-            parser = Parser(self.imdb_url)
-            parser.get_elem_2()
-    
+
+            self.parser.get_elem_2()
+
     def test_get_elem_2_returns_none_when_elem_not_found(self):
-        parser = Parser(self.imdb_url)
-        parser.elem_class_2 = {"tag": "div",
-                                     "class": "non-existant-class"}
-        self.assertIsNone(parser.get_elem_2())
+        self.parser.elem_class_2 = {"tag": "div",
+                                    "class": "non-existant-class"}
+        self.assertIsNone(self.parser.get_elem_2())
 
     def test_get_value_2(self):
-        parser = Parser(self.rt_url)
-        parser.elem_class_2 = {"tag": "span",
-                                     "class": "mop-ratings-wrap__percentage"}
+        self.parser.elem_class_2 = {"tag": "span",
+                                    "class": "score-element-two-wrapper"}
 
-        self.assertEqual(parser.get_value_2(), "78%")
-    
+        self.assertEqual(self.parser.get_value_2(), "93%")
+
     def test_get_value_2_returns_none_when_elem_not_found(self):
-        parser = Parser(self.imdb_url)
-        parser.elem_class_2 = {"tag": "div",
-                                     "class": "non-existant-class"}
-        self.assertIsNone(parser.get_value_2())
+        self.parser.elem_class_2 = {"tag": "div",
+                                    "class": "non-existant-class"}
+        self.assertIsNone(self.parser.get_value_2())
