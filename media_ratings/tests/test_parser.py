3adfcbfb98eed89d88c7a29e4b3eed48d5c96f5c
Manuel <manuelpinedacabeza@gmail.com>
2022-06-24 10:56:36 +0100

    Renamed test_url_cannot_be_false to test_url_cannot_be_empty. Renamed test_get_page_source to test_get_page_source_returns_mock_html_page. Renamed test_soup to test_get_page_source_returns_html_page_from_network.

--- media_ratings/tests/test_parser.py
+++ media_ratings/tests/test_parser.py
@@ -24,17 +24,19 @@ class ParserTests(TestCase):
     def setUp(self):
         self.parser = Parser(html_page=html_page)
 
-    def test_url_cannot_be_false(self):
+    def test_url_cannot_be_empty(self):
         with self.assertRaises(TypeError):
-            url = False
+            url = ""
             parser = Parser(url)
 
-    def test_get_page_source(self):
-        self.assertInHTML("<title>Mock Page</title>",
+    def test_get_page_source_returns_mock_html_page(self):
+        self.assertInHTML(html_page,
                           str(self.parser._get_page_source()))
 
-    def test_soup(self):
-        self.assertIn("<title>Mock Page</title>", str(self.parser.soup))
+    def test_get_page_source_returns_html_page_from_network(self):
+        parser = Parser("http://www.google.com")
+        self.assertInHTML("<title>Google</title>",
+                          str(parser._get_page_source()))
 
     def test_get_elem_1(self):
         self.parser.elem_class_1 = {"tag": "div",
@@ -45,7 +47,6 @@ class ParserTests(TestCase):
 
     def test_elem_class_1_not_assigned_error(self):
         with self.assertRaises(AttributeError):
-
             self.parser.get_elem_1()
 
     def test_get_elem_1_returns_none_when_elem_not_found(self):
@@ -71,7 +72,6 @@ class ParserTests(TestCase):
 
     def test_elem_class_2_not_assigned_error(self):
         with self.assertRaises(AttributeError):
-
             self.parser.get_elem_2()
 
     def test_get_elem_2_returns_none_when_elem_not_found(self):
