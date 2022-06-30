96840c44e0df574bb6098e9f12021151786b0ca6
Manuel <manuelpinedacabeza@gmail.com>
2022-06-30 17:55:54 +0100

    Uncommeneted out code

--- e2e/cypress/integration/ui-tests/scoreboard.spec.js
+++ e2e/cypress/integration/ui-tests/scoreboard.spec.js
@@ -9,11 +9,11 @@ const testContexts = [
 ];
 
 const nonExistantSeries = [
-  // "Buffy of Thrones",
+  "Buffy of Thrones",
   "Breaking Anatomy",
-  // "South Mirror",
-  // "Stranger Zone",
-  // "Better Call MacGyver",
+  "South Mirror",
+  "Stranger Zone",
+  "Better Call MacGyver",
 ];
 
 const testAllScoreValues = (imdbScore, tomatometerScore, audienceScore) => {
@@ -46,95 +46,95 @@ const testAllScoreValues = (imdbScore, tomatometerScore, audienceScore) => {
     .and("contain.text", audienceScore);
 };
 
-// for (const testContext of testContexts) {
-//   for (const serie in series) {
-//     const currentTestData = testData[serie];
+for (const testContext of testContexts) {
+  for (const serie in series) {
+    const currentTestData = testData[serie];
 
-//     if (testContext === "Scoreboard - Loading icons test") {
-//       describe(testContext, () => {
-//         before(() => {
-//           cy.intercept("http://localhost:8000/fetch-score-data/*", {});
-//           cy.visit("/");
-//           cy.get("#id_search").type(serie);
-//           cy.get("[data-test=search-button]").should("be.visible").click();
-//         });
+    if (testContext === "Scoreboard - Loading icons test") {
+      describe(testContext, () => {
+        before(() => {
+          cy.intercept("http://localhost:8000/fetch-score-data/*", {});
+          cy.visit("/");
+          cy.get("#id_search").type(serie);
+          cy.get("[data-test=search-button]").should("be.visible").click();
+        });
 
-//         it("should show loading icons before data is fetched", () => {
-//           cy.get("#imdb-score > .spinner-border").should("be.visible");
-//           cy.get("#tomatometer-value > .spinner-border").should("be.visible");
-//           cy.get("#audience_score-value > .spinner-border").should(
-//             "be.visible"
-//           );
-//         });
-//       });
-//     } else {
-//       describe(`${testContext} for ${currentTestData.title}`, () => {
-//         beforeEach(() => {
-//           cy.visit("/");
-//           cy.get("#id_search").type(currentTestData.title);
-//           cy.get("[data-test=search-button]").should("be.visible").click();
-//         });
+        it("should show loading icons before data is fetched", () => {
+          cy.get("#imdb-score > .spinner-border").should("be.visible");
+          cy.get("#tomatometer-value > .spinner-border").should("be.visible");
+          cy.get("#audience_score-value > .spinner-border").should(
+            "be.visible"
+          );
+        });
+      });
+    } else {
+      describe(`${testContext} for ${currentTestData.title}`, () => {
+        beforeEach(() => {
+          cy.visit("/");
+          cy.get("#id_search").type(currentTestData.title);
+          cy.get("[data-test=search-button]").should("be.visible").click();
+        });
 
-//         it("should display current serie Title, IMDb and Rottentoes data", () => {
-//           console.log(currentTestData);
-//           cy.get("[data-test=media-title]")
-//             .should("be.visible")
-//             .and("contain.text", currentTestData.title);
+        it("should display current serie Title, IMDb and Rottentoes data", () => {
+          console.log(currentTestData);
+          cy.get("[data-test=media-title]")
+            .should("be.visible")
+            .and("contain.text", currentTestData.title);
 
-//           testAllScoreValues(
-//             currentTestData.imdb,
-//             currentTestData.rt.tomatometer,
-//             currentTestData.rt.audience_score
-//           );
-//         });
-//       });
-//     }
-//   }
-// }
+          testAllScoreValues(
+            currentTestData.imdb,
+            currentTestData.rt.tomatometer,
+            currentTestData.rt.audience_score
+          );
+        });
+      });
+    }
+  }
+}
 
-// for (const serie in series) {
-//   const currentTestData = testData[serie];
+for (const serie in series) {
+  const currentTestData = testData[serie];
 
-//   describe("Scoreboard - Parcial data tests", () => {
-//     it("should display N/A for imdb score data and rottentomatoes data", () => {
-//       const currentTestDataNoIMDB = Object.assign({}, currentTestData);
-//       currentTestDataNoIMDB["imdb"] = "N/A";
+  describe("Scoreboard - Parcial data tests", () => {
+    it("should display N/A for imdb score data and rottentomatoes data", () => {
+      const currentTestDataNoIMDB = Object.assign({}, currentTestData);
+      currentTestDataNoIMDB["imdb"] = "N/A";
 
-//       cy.intercept(
-//         "http://localhost:8000/fetch-score-data/*",
-//         currentTestDataNoIMDB
-//       );
-//       cy.visit("/");
-//       cy.get("#id_search").type(serie);
-//       cy.get("[data-test=search-button]").should("be.visible").click();
+      cy.intercept(
+        "http://localhost:8000/fetch-score-data/*",
+        currentTestDataNoIMDB
+      );
+      cy.visit("/");
+      cy.get("#id_search").type(serie);
+      cy.get("[data-test=search-button]").should("be.visible").click();
 
-//       testAllScoreValues(
-//         "N/A",
-//         currentTestDataNoIMDB.rt.tomatometer,
-//         currentTestDataNoIMDB.rt.audience_score
-//       );
-//     });
-//     it("should display score data for imdb score data and N/A for rottentomatoes data", () => {
-//       const currentTestDataNoRT = Object.assign({}, currentTestData);
-//       currentTestDataNoRT["rt"]["tomatometer"] = "N/A";
-//       currentTestDataNoRT["rt"]["audience_score"] = "N/A";
+      testAllScoreValues(
+        "N/A",
+        currentTestDataNoIMDB.rt.tomatometer,
+        currentTestDataNoIMDB.rt.audience_score
+      );
+    });
+    it("should display score data for imdb score data and N/A for rottentomatoes data", () => {
+      const currentTestDataNoRT = Object.assign({}, currentTestData);
+      currentTestDataNoRT["rt"]["tomatometer"] = "N/A";
+      currentTestDataNoRT["rt"]["audience_score"] = "N/A";
 
-//       cy.intercept(
-//         "http://localhost:8000/fetch-score-data/*",
-//         currentTestDataNoRT
-//       );
-//       cy.visit("/");
-//       cy.get("#id_search").type(serie);
-//       cy.get("[data-test=search-button]").should("be.visible").click();
+      cy.intercept(
+        "http://localhost:8000/fetch-score-data/*",
+        currentTestDataNoRT
+      );
+      cy.visit("/");
+      cy.get("#id_search").type(serie);
+      cy.get("[data-test=search-button]").should("be.visible").click();
 
-//       cy.get("[data-test=imdb-header]")
-//         .should("be.visible")
-//         .and("contain.text", "IMDb");
+      cy.get("[data-test=imdb-header]")
+        .should("be.visible")
+        .and("contain.text", "IMDb");
 
-//       testAllScoreValues(currentTestDataNoRT.imdb, "N/A", "N/A");
-//     });
-//   });
-// }
+      testAllScoreValues(currentTestDataNoRT.imdb, "N/A", "N/A");
+    });
+  });
+}
 
 for (const serie of nonExistantSeries) {
   describe(`Scoreboard - Non-existing series: (${serie}) tests`, () => {
