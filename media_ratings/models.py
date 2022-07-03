c81589135619d6767663aecb571e3cf0319f34b8
Manuel <manuelpinedacabeza@gmail.com>
2022-07-03 12:39:25 +0100

    Added docstrings. Added get_formatted_scores to IMDbScores and RottentomatoesScores.

--- media_ratings/models.py
+++ media_ratings/models.py
@@ -3,12 +3,15 @@ from django.db import models
 # Create your models here.
 
 class Media(models.Model):
-    """Base class for representing a media instance.
-    Should not be used directly and extended instead."""
-    
+    """
+    Base class for representing a media instance.
+
+    Should not be used directly but extended instead.
+    """
+
     title = models.CharField(max_length=100, unique=True, blank=False, null=False)
     description = models.TextField(blank=True)
-    
+
     def imdb_scores(self):
         imdb_score = None
         imdb_score_instance = IMDbScores.objects.filter(media=self).first()
@@ -34,7 +37,6 @@ class Media(models.Model):
           "tomatometer": f"{tomatometer_score}%" if tomatometer_score else 'N/A',
           "audience_score": f"{audience_score}%" if audience_score else 'N/A'
         }
-    
 
     def __str__(self):
         return self.title
@@ -47,24 +49,35 @@ class TV_Series(Media):
 
     class Meta:
         verbose_name_plural = "TV Series"
-    
+
+
 class MediaScore(models.Model):
-    """Base class for representing a media scores.
-    Should not be used directly and extended instead."""
+    """
+    Base class for representing a media scores.
     
+    Should not be used directly but extended instead.
+    """
+
     media = models.ForeignKey(Media, on_delete=models.CASCADE)
-    
+
     def __str__(self):
         return f"{Media} scores"
 
     class Meta:
         verbose_name_plural = "Media Scores"
-    
+
+
 class IMDbScores(MediaScore):
     imdb_score = models.FloatField(null=True)
 
     class Meta:
         verbose_name_plural = "IMDb Scores"
+
+    def get_formatted_scores(self):
+        return {
+            "imdb_score": f"{self.imdb_score}/10" if self.imdb_score else "N/A"
+        }
+
 
 class RottentomatoesScores(MediaScore):
     tomatometer_score = models.IntegerField(null=True)
@@ -72,3 +85,9 @@ class RottentomatoesScores(MediaScore):
 
     class Meta:
         verbose_name_plural = "Rottentomatoes Scores"
+
+    def get_formatted_scores(self):
+        return {
+          "tomatometer": f"{self.tomatometer_score}%" if self.tomatometer_score else 'N/A',
+          "audience_score": f"{self.audience_score}%" if self.audience_score else 'N/A'
+        }
