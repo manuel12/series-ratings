997280124e84eb8e20eca709f280498348a2d2dc
Manuel <manuelpinedacabeza@gmail.com>
2022-07-04 16:32:23 +0100

    Added dosctrings

--- media_ratings/utils.py
+++ media_ratings/utils.py
@@ -12,20 +12,24 @@ def save_json_data(file, data):
         json.dump(data, f)
 
 
-def capitalize_phrase(phrase):
+def capitalize_phrase(phrase=""):
+    """Capitalizes all words in a phrase."""
     raise_attribute_error_if_missing(phrase)
     return " ".join([word.capitalize() for word in phrase.split(" ")])
 
 
-def sanitize_phrase(phrase):
+def sanitize_phrase(phrase=""):
+    """Converts '&'s to 'and's and hyphens to spaces."""
     raise_attribute_error_if_missing(phrase)
     return "".join(char.replace("&", "and").replace("-", " ") for char in phrase)
 
 
-def standardize_phrase(phrase):
+def standardize_phrase(phrase=""):
+    """Capitalizes and sanitizes phrases."""
     return capitalize_phrase(sanitize_phrase(phrase))
 
 
 def raise_attribute_error_if_missing(phrase=None):
+    """Raises error if phrase is missing or falsey."""
     if not phrase:
         raise AttributeError("The search term cannot be empty.")
