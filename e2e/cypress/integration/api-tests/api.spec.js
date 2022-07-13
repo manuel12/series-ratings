7f61b3490ca8e83d5c08d37272e2adbd18ef6244
Manuel <manuelpinedacabeza@gmail.com>
2022-07-13 20:31:32 +0100

    Added should return score data for  and should return score data not found tests for 5 series.

--- e2e/cypress/integration/api-tests/api.spec.js
+++ e2e/cypress/integration/api-tests/api.spec.js
@@ -1,18 +1,55 @@
 /// <reference types="cypress" />
+let testData = require("../../fixtures/test-data.json");
+let seriesTitles = testData.map((serie) => serie.title);
 
-describe("Search tests", () => {
-    beforeEach(() => {
-      cy.visit("/");
+describe("Fetch Score Data API 'GET' request", () => {
+  for (const serieTitle of seriesTitles) {
+    it(`should return score data for ${serieTitle}`, () => {
+      const searchTerm = serieTitle;
+      cy.request({
+        method: "GET",
+        url: `http://localhost:8000/fetch-score-data/?media=${searchTerm}`,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.headers).to.have.property(
+          "content-type",
+          "application/json"
+        );
+        const currentSeries = testData.filter(
+          (_serie) => _serie.title === serieTitle
+        )[0];
+        const imdbScore = currentSeries.imdb;
+        const tomatometerScore = currentSeries.rt.tomatometer;
+        const audienceScore = currentSeries.rt.audience_score;
+
+        expect(response.body.imdb).to.eq(imdbScore);
+        expect(response.body.rt.tomatometer).to.eq(tomatometerScore);
+        expect(response.body.rt.audience_score).to.eq(audienceScore);
+      });
     });
+  }
+  it("should return score data not found", () => {
+    const searchTerm = "Non Existing Movie";
+    cy.request({
+      method: "GET",
+      url: `http://localhost:8000/fetch-score-data/?media=${searchTerm}`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((response) => {
+      console.log(response);
+      expect(response.status).to.eq(200);
+      expect(response.headers).to.have.property(
+        "content-type",
+        "application/json"
+      );
 
-    it("should send correct chart data(api test) for the chart", () => {
-        cy.intercept("POST", "/results/").as("results");
-
-        cy.get("[type=text]").type("Stranger Things");
-        cy.get("[data-test=search-button]").click();
-
-        cy.wait("@results").its("response.statusCode").should("eq", 200);
-    })
-  
+      expect(response.body.imdb).to.eq("N/A");
+      expect(response.body.rt.tomatometer).to.eq("N/A");
+      expect(response.body.rt.audience_score).to.eq("N/A");
+    });
   });
-  
+});
