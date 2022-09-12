b70ace21ef18fa4da9479c0a7f5ea81ef9563def
Manuel <manuelpinedacabeza@gmail.com>
2022-09-12 19:01:53 +0200

    Added api urls

--- media_rating/urls.py
+++ media_rating/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('media_ratings.urls')),
+    path('api/', include("api.urls")),
     path('admin/', admin.site.urls),
 ]
