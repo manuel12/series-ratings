82d9665758e1ab6db5304119d53a4512a4d7729c
Manuel <manuelpinedacabeza@gmail.com>
2022-09-12 19:03:09 +0200

    Removed unused views.

--- media_ratings/views.py
+++ media_ratings/views.py
@@ -1,42 +1,7 @@
-from django.http import JsonResponse
-from django.shortcuts import render, redirect
-from .forms import SearchForm
-from .utils import capitalize_phrase, sanitize_phrase
-from .score_fetcher import ScoreFetcher
-
+from django.shortcuts import render
 
 # Create your views here.
 
 
-def homepage(request):
-    template = "search.html"
-    form = SearchForm()
-
-    context = {'form': form}
-    return render(request, template, context)
-
-
-def scoreboard(request):
-    search_term = request.POST.get('search')
-    template = "scoreboard.html"
-
-    try:
-        capitalized_search_term = capitalize_phrase(search_term)
-        sanitized_search_term = sanitize_phrase(search_term)
-        print(sanitized_search_term)
-    except AttributeError:
-        return redirect("media_ratings:home")
-
-    context = {
-        "media_title": capitalized_search_term,
-        "search_term": sanitized_search_term
-    }
-    return render(request, template, context)
-
-
-def fetch_score_data(request):
-    search_term = request.GET.get("media")
-
-    score_fetcher = ScoreFetcher(search_term)
-    score_data = score_fetcher.get_score_data()
-    return JsonResponse(score_data)
+def home(request):
+    return render(request, "build/index.html") 
