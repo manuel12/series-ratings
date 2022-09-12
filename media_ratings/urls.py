70a24dfa51df8714e0d9301524a68c0ecc343ce0
Manuel <manuelpinedacabeza@gmail.com>
2022-09-12 19:02:46 +0200

    Removed unused urls

--- media_ratings/urls.py
+++ media_ratings/urls.py
@@ -4,7 +4,5 @@ from media_ratings import views
 app_name = 'media_ratings'
 
 urlpatterns = [
-    path('', views.homepage, name='home'),
-    path('scoreboard/', views.scoreboard, name='scoreboard'),
-    path('fetch-score-data/', views.fetch_score_data, name='fetch-score-data')
+    path('', views.home, name='home')
 ]
