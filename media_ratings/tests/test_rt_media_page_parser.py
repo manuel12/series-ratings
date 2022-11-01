82dc61b1bc5dddb09e0a895315840db10163b2cd
Manuel <manuelpinedacabeza@gmail.com>
2022-11-01 21:58:40 +0100

    Added print with test case name. Updated clean_up_parse_value test to assert type. Updated value tests and tomatometer and audience score tests to assert score values are within a range instead of matching them to a hardcoded value.

--- media_ratings/tests/test_rt_media_page_parser.py
+++ media_ratings/tests/test_rt_media_page_parser.py
@@ -4,21 +4,24 @@ from media_ratings.parsers import RottentomatoesMediaPageParser
 
 class RottentomatoesMediaPageParserTests(TestCase):
     def setUp(self):
+        print("RottentomatoesMediaPageParserTests")
+
         # love, death and robots
         self.rt_url = "https://www.rottentomatoes.com/tv/the-wire"
         self.parser = RottentomatoesMediaPageParser(self.rt_url)
 
     def test_clean_up_parsed_value(self):
         cleaned_up_value = self.parser._clean_up_parsed_value("94%")
+        self.assertTrue(isinstance(cleaned_up_value, int))
         self.assertEqual(cleaned_up_value, 94)
 
     def test_get_tomatometer_value(self):
         tomatometer_value = self.parser.get_tomatometer_value()
-        self.assertEqual(tomatometer_value, 94)
+        self.assertTrue(0 < tomatometer_value < 101)
 
     def test_get_audience_score_value(self):
         audience_score_value = self.parser.get_audience_score_value()
-        self.assertEqual(audience_score_value, 97)
+        self.assertTrue(0 < audience_score_value < 101)
 
     def test_get_elem_1(self):
         self.assertIn("mop-ratings-wrap__percentage",
@@ -35,7 +38,10 @@ class RottentomatoesMediaPageParserTests(TestCase):
         self.assertIsNone(self.parser.get_elem_1())
 
     def test_get_value_1(self):
-        self.assertEqual(self.parser.get_value_1(), "94%")
+        self.assertTrue(isinstance(self.parser.get_value_1(), str))
+        self.assertTrue("%" in self.parser.get_value_1())
+        clean_value = int(self.parser.get_value_1().replace("%", ""))
+        self.assertTrue(0 < clean_value < 101)
 
     def test_get_value_1_returns_none_when_elem_not_found(self):
         self.parser.elem_class_1 = {"tag": "div",
@@ -57,7 +63,10 @@ class RottentomatoesMediaPageParserTests(TestCase):
         self.assertIsNone(self.parser.get_elem_2())
 
     def test_get_value_2(self):
-        self.assertEqual(self.parser.get_value_2(), "97%")
+        self.assertTrue(isinstance(self.parser.get_value_2(), str))
+        self.assertTrue("%" in self.parser.get_value_2())
+        clean_value = int(self.parser.get_value_2().replace("%", ""))
+        self.assertTrue(0 < clean_value < 101)
 
     def test_get_value_2_returns_none_when_elem_not_found(self):
         self.parser.elem_class_2 = {"tag": "div",
