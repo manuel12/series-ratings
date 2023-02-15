dbee661749f4b036f4a2702e152ec819b00b7e14
Manuel <manuelpinedacabeza@gmail.com>
2023-02-15 12:02:39 +0100

    renamed header to header-container

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -2,7 +2,7 @@ import classes from "../css/Header.module.css";
 
 const Header = () => {
   return (
-    <div className={classes["header"]} data-test="header">
+    <div className={classes["header-container"]} data-test="header">
       <a href="/" className="text-decoration-none" data-test="header-text">
         <h1 className="text-light text-center display-1">Series Ratings</h1>
       </a>
