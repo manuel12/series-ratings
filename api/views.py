ab966306d1fe12a726c7f78cab90bc1e259b7feb
Manuel <manuelpinedacabeza@gmail.com>
2023-06-02 14:36:35 +0200

    Refactored json response, added score data and title to response.

--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.http import JsonResponse
-from media_ratings.score_fetcher import ScoreFetcher
+from media_ratings.score_fetcher import get_score_data
 
 # Create your views here.
 
@@ -11,10 +11,9 @@ def fetch_score_data(request):
     search_term = request.GET.get("media")
 
     if search_term:
-        score_fetcher = ScoreFetcher(search_term)
-        standardized_search_term = score_fetcher.get_starndardized_search_term()
-        print(f"-- SEARCH TERM: {standardized_search_term}")
-        score_data = score_fetcher.get_score_data()
-        print(score_data)
-        return JsonResponse(score_data, status=200, safe=False)
+        score_data, search_term = get_score_data(search_term)
+        print(f"-- {score_data}, {search_term}")
+        return JsonResponse(
+            {"data": score_data, "title": search_term},
+            status=200, safe=False)
     return Response({}, status=200)
