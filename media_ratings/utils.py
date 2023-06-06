3c97427d5cf7046b404d60a3f2663785290999fc
Manuel <manuelpinedacabeza@gmail.com>
2023-06-06 17:27:31 +0200

    Added functions to be used in parsers.

--- media_ratings/utils.py
+++ media_ratings/utils.py
@@ -1,3 +1,4 @@
+import urllib.request
 import json
 
 
@@ -33,3 +34,31 @@ def raise_attribute_error_if_missing(phrase=None):
     """Raises error if phrase is missing or falsey."""
     if not phrase:
         raise AttributeError("The search term cannot be empty.")
+
+
+def add_pluses_to_url(url):
+    """
+    Returns a string url with spaces replaced with pluses.
+    Url before: https://www.imdb.com/title/love death and-robots/
+    Url after: https://www.imdb.com/title/love+death+and+robots/
+    """
+    print(f"-- Adding pluses url: [ {url} ]...")
+    updated_url = url.strip().replace(" ", "+").replace("-", "+")
+    print(f"-- Cleaned url: [ {updated_url} ]...")
+    return updated_url
+
+
+def get_page_source(url):
+    """
+    Returns the html  source of the page.
+    """
+    print(f"-- Opening: [ {url} ]...")
+    # It seems like IMDb does not like the user agent of Python 3.x so 403 error is shown.
+    # Specifying User-Agent solves the problem.
+    req = urllib.request.Request(
+        url, headers={
+            'Accept-Language': 'en-US,en;q=0.5',
+            'User-Agent': 'Mozilla/5.0'
+        })
+    page = urllib.request.urlopen(req)
+    return page.read()
